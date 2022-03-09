b55a24bb9e7a452924a533b33ccfdf2dd8ac8fcf
camillapalombi <camilla.palombi14@gmail.com>
2022-03-09 14:59:16 +0100

    JS Implementati bonus e correzioni

--- js/script.js
+++ js/script.js
@@ -59,55 +59,111 @@ const posts = [
 ];
 
 
-//1. stampo i post nel'DOM tramite JS:
-
+//1. stampo i post nel DOM tramite JS:
+const arrLikedIds = []; //array ID
 const containerPosts = document.getElementById('container'); //container di tutti i post
 
-function createPost(postKey) { //fuzione creazione post che richiamo in fondo!!
+function createPost(objPost) { //fuzione creazione post che richiamo in fondo!!
     
     const postElement = document.createElement('div');
     postElement.classList.add('post');
-    postElement.innerHTML = (`<div class="post__header">
-            <div class="post-meta">                    
-                <div class="post-meta__icon">
-                    <img class="profile-pic" src="${postKey.author.image}" alt="${postKey.author.name}">                    
-                </div>
-                <div class="post-meta__data">
-                    <div class="post-meta__author">${postKey.author.name}</div>
-                    <div class="post-meta__time">4 mesi fa</div>
-                </div>                    
-            </div>
+    postElement.dataset.postid = objPost.id;
+    postElement.innerHTML = `<div class="post__header">
+    <div class="post-meta">
+        <div class="post-meta__icon">
+            ${getProfilePicHtml(objPost)}
         </div>
-            <div class="post__text">${postKey.content}</div>
-            <div class="post__image">
-            <img src="${postKey.media}" alt="">
-            </div>
-            <div class="post__footer">
-            <div class="likes js-likes">
-                <div class="likes__cta">
-                    <a class="like-button  js-like-button" href="#!" data-postid="1">
-                        <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
-                        <span class="like-button__label">Mi Piace</span>
-                    </a>
-                </div>
-                <div class="likes__counter">
-                    Piace a <b id="like-counter-1" class="js-likes-counter">${postKey.likes}</b> persone
-                </div>
-            </div> 
-        </div>`);
+        <div class="post-meta__data">
+            <div class="post-meta__author">${objPost.author.name}</div>
+            <div class="post-meta__time">${objPost.created.split('-').reverse().join('/')}</div>
+        </div>
+    </div>
+</div>
+<div class="post__text">${objPost.content}</div>
+<div class="post__image">
+    <img src="${objPost.media}" alt="">
+</div>
+<div class="post__footer">
+    <div class="likes js-likes">
+        <div class="likes__cta">
+            <a class="like-button js-like-button ${arrLikedIds.includes(objPost.id) ? 'like-button--liked': ''}" href="#">
+                <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                <span class="like-button__label">Mi Piace</span>
+            </a>
+        </div>
+        <div class="likes__counter">
+            Piace a <b class="js-likes-counter">${objPost.likes}</b> persone
+        </div>
+    </div>
+</div>`;
 
-    let btnLike = postElement.querySelector('.like-button'); //bottone MI PIACE
-
-    btnLike.addEventListener('click', clickButton); //richiamo funzione click MI PIACE
-
-    function clickButton() { //funzione cambio colore al click di MI PIACE
-    
-        this.classList.toggle('like-button--liked'); //aggingo/tolgo classe color verde al click
-
-    }
-
+    postElement.querySelector('.like-button').addEventListener('click', clickButton);//bottone MI PIACE
+ 
     containerPosts.append(postElement); //stampo i post nel DOM
 
+}
+
+
+//funzione cambiamento al click di MI PIACE
+function clickButton(event) { 
+    
+    event.preventDefault();
+    const btnLike = this;
+    const postElement = btnLike.closest('.post');
+    const postId = parseInt(postElement.dataset.postid);
+    const eleCounter = postElement.querySelector('.js-likes-counter');
+
+    let indexLikedPosts = 0;
+
+    while (postId != posts[indexLikedPosts].id) {
+		indexLikedPosts++;
+	}
+	const objPost = posts[indexLikedPosts];
+
+    if (btnLike.classList.contains('like-button--liked')) {
+		removeLike(btnLike, objPost);
+	} else {
+		addLike(btnLike, objPost);
+	}
+
+    eleCounter.innerHTML = objPost.likes;
+         
+}
+
+
+//se tolgo LIKE
+function removeLike(btnLikeArgument, objPostArgument) {
+	btnLikeArgument.classList.remove('like-button--liked');
+
+	objPostArgument.likes--;
+	const index = arrLikedIds.indexOf(objPostArgument.id);
+	arrLikedIds.splice(index, 1);
+	
+}
+
+
+//se metto LIKE
+function addLike(btnLikeArgument, objPostArgument) {
+	btnLikeArgument.classList.add('like-button--liked');
+
+	objPostArgument.likes++;
+	arrLikedIds.push(objPostArgument.id);
+	
+}
+
+
+// funzione x gestire assenza immagine di profilo
+function getProfilePicHtml(objPost) {
+	if (objPost.author.image == null) {
+		let arrNameParts = objPost.author.name.split(' ');
+		let initials = '';
+		for (let index in arrNameParts) {
+			initials += arrNameParts[index][0].toUpperCase();
+		}
+		return `<div class="profile-pic-default"><span>${initials}</span></div>`;
+	} else {
+		return `<img class="profile-pic" src="${objPost.author.image}" alt="${objPost.author.name}"></img>`;
+	}
 }
 
     //CICLO X LA CREAZIONE DEI 5 POST
@@ -115,16 +171,3 @@ function createPost(postKey) { //fuzione creazione post che richiamo in fondo!!
     
         createPost(posts[i]); //richiamo funzione creazione post
     }
-
-
-
-
-
-
-
-
-
-
-
-
-
